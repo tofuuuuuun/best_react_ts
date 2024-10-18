24aafc8a45e4c5f00bc27cc49494afce0fbf6993
hirano <hirakenlp21@gmail.com>
2024-10-18 23:12:15 +0900

    型解決のため、ismodalopenを追加

--- app/frontend/src/components/AddButton.tsx
+++ app/frontend/src/components/AddButton.tsx
@@ -1,12 +1,13 @@
 type AddButtonProps = {
+    isModalOpen: boolean;
     setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const AddButton = (props: AddButtonProps) => {
-    const { setModalIsOpen } = props;
+    const { setModalIsOpen, isModalOpen } = props;
     return (
         <div className='albumAddButton'>
-            <button className='l-albumArt albumAddButton addButton action' onClick={() => { setModalIsOpen(prevState => !prevState) }}>
+            <button className='l-albumArt albumAddButton addButton action' onClick={() => setModalIsOpen(!isModalOpen)}>
                 <span className='icon-add'></span>
             </button>
         </div>
