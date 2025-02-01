df818a5d0ae0a1f81f579dfb3d52623c8f2789bd
hirano <hirakenlp21@gmail.com>
2025-02-01 23:14:45 +0900

    ヘッダーコンポーネントにホームロゴを追加し、ロゴ切り替え機能を実装

--- app/frontend/src/common/Header.tsx
+++ app/frontend/src/common/Header.tsx
@@ -1,10 +1,12 @@
+import homeLogoIcon from '@/images//homeLogo.svg';
 import albumLogoIcon from '@/images/album/logo.svg';
 import movieLogoIcon from '@/images/movie/logo.svg';
 import { headerProps } from "@/types/types";
 
 export const Header = (props: headerProps) => {
     const { type } = props;
-    const logoIcon = type === 'album' ? albumLogoIcon : movieLogoIcon;
+    const logoIcon = type === 'album' ? albumLogoIcon : type === 'movie' ? movieLogoIcon : homeLogoIcon;
+
     return (
         <>
             <header>
