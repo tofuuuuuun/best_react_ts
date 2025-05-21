e318ac76e46b5707f936457dfbd7e3bd7c1cdd77
hirano <hirakenlp21@gmail.com>
2025-05-21 22:30:36 +0900

    rootのpathを設定

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -14,18 +14,18 @@ export const App = () => {
     <>
       <main>
         <Routes location={location}>
+          <Route path="/" element={
+            <>
+              <Header type={TYPE} />
+              <ul className='linkWrapper'>
+                <li className='l-linkCard'><Link to='/album' className='linkCard ta-center'><img src={albumLogoIcon}></img></Link></li>
+                <li className='l-linkCard'><Link to='/movie' className='linkCard ta-center'><img src={movieLogoIcon}></img></Link></li>
+              </ul>
+            </>
+          } />
           <Route path='/album' element={<AlbumApp key={location.pathname} />} />
           <Route path='/movie' element={<MovieApp key={location.pathname} />} />
         </Routes>
-        {location.pathname === '/' && (
-          <>
-            <Header type={TYPE} />
-            <ul className='linkWrapper'>
-              <li className='l-linkCard'><Link to='/album' className='linkCard ta-center'><img src={albumLogoIcon}></img></Link></li>
-              <li className='l-linkCard'><Link to='/movie' className='linkCard ta-center'><img src={movieLogoIcon}></img></Link></li>
-            </ul>
-          </>
-        )}
       </main >
     </>
   )
