3351917bd5badeea8cfa1219944fa193d36cdda4
hirano <hirakenlp21@gmail.com>
2025-05-24 21:15:10 +0900

    イラスト要素とメインページ用のテキストを追加

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { AlbumApp } from '@/album/components/AlbumApp';
 import { Header } from '@/common/Header';
 import '@/css/home.css';
 import albumLogoIcon from '@/images/album/logo.svg';
+import mainImage from '@/images/home/main.png';
 import movieLogoIcon from '@/images/movie/logo.svg';
 import { MovieApp } from '@/movie/MovieApp';
 import { Link, Route, Routes, useLocation } from 'react-router-dom';
@@ -17,6 +18,16 @@ export const App = () => {
           <Route path="/" element={
             <>
               <Header type={TYPE} />
+              <div className='l-container mainContainer'>
+                <h2>好きなものを語りたいときに、ちょっと便利なリスト。</h2>
+                <p className='textM1'>このサイトは、テーマごとに好きな作品を10個だけ選んで並べるWebサービスです。</p>
+                <p className='textM1 m-bottom-1em'>「ベストアルバム10枚」「泣ける映画10選」など‥‥ 自分だけのリストを気軽につくれます。</p>
+                <p className='textM1 m-bottom-1em'>ユーザー登録は必要ありません。</p>
+                <p className='textM1 m-bottom-1em'>作品のカバー画像も自動で表示されるので、見た目にもわかりやすく。</p>
+                <p className='textM1 m-bottom-1em'>話のきっかけや、誰かとの共有にも使えます。</p>
+                <p className='textM1 m-bottom-1em'>ただ並べてみるだけでも、あなたの「これが好き」が少しはっきり見えてくるかもしれません。</p>
+                <img src={mainImage} alt='mainImage' className='mainImage' />
+              </div>
               <ul className='linkWrapper'>
                 <li className='l-linkCard'><Link to='/album' className='linkCard ta-center'><img src={albumLogoIcon}></img></Link></li>
                 <li className='l-linkCard'><Link to='/movie' className='linkCard ta-center'><img src={movieLogoIcon}></img></Link></li>
