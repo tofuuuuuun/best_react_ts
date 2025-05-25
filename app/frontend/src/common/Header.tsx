288cf312b20ff455a1f58125a1427e5858f2b615
hirano <hirakenlp21@gmail.com>
2025-05-25 21:35:11 +0900

    ヘッダーのリンク追加

--- app/frontend/src/common/Header.tsx
+++ app/frontend/src/common/Header.tsx
@@ -1,7 +1,11 @@
+import { AlbumApp } from '@/album/components/AlbumApp';
+import { App } from '@/App';
 import homeLogoIcon from '@/images//homeLogo.svg';
 import albumLogoIcon from '@/images/album/logo.svg';
 import movieLogoIcon from '@/images/movie/logo.svg';
+import { MovieApp } from '@/movie/MovieApp';
 import { headerProps } from "@/types/types";
+import { Link, Route, Routes } from 'react-router-dom';
 
 export const Header = (props: headerProps) => {
     const { type } = props;
@@ -11,6 +15,20 @@ export const Header = (props: headerProps) => {
         <>
             <header className="l-header ta-center">
                 <h1 className='logoPosition'><img src={logoIcon} alt="best" className="headerLogo" /></h1>
+                <Routes location={location}>
+                    <Route path="/" element={
+                        <>
+                            <ul className='linkWrapper'>
+                                <li className='l-linkCard'><Link to='/' className='linkCard ta-center'>TOP</Link></li>
+                                <li className='l-linkCard'><Link to='/album' className='linkCard ta-center'>MUSIC</Link></li>
+                                <li className='l-linkCard'><Link to='/movie' className='linkCard ta-center'>MOVIE</Link></li>
+                            </ul>
+                        </>
+                    } />
+                    <Route path='/' element={<App key={location.pathname} />} />
+                    <Route path='/album' element={<AlbumApp key={location.pathname} />} />
+                    <Route path='/movie' element={<MovieApp key={location.pathname} />} />
+                </Routes>
             </header>
         </>
     );
