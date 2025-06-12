f4b642d30c08d1466d1cc43c771055febad01214
hirano <hirakenlp21@gmail.com>
2025-06-12 21:36:06 +0900

    バッチ処理の開始と終了時にログを記録するように変更

--- app/php/src/saveImages.sh
+++ app/php/src/saveImages.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+echo "start batch" > /tmp/batch.log
 while true; do
   php /var/www/html/movie/saveTopMovieImage.php
   php /var/www/html/album/saveTopAlbumImage.php
   sleep 3600
 done
+echo "end batch" >> /tmp/batch.log
