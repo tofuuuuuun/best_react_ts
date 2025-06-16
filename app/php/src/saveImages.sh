e9b847ace5cacddd02eb348b974b72259562ece8
hirano <hirakenlp21@gmail.com>
2025-06-16 21:49:58 +0900

    ログ出力を改善するために、whoami、id、/var/www/htmlのディレクトリリストを追加

--- app/php/src/saveImages.sh
+++ app/php/src/saveImages.sh
@@ -4,6 +4,10 @@ while true; do
   which php >> /tmp/batch.log
   php -v >> /tmp/batch.log
   env >> /tmp/batch.log
+  whoami > /tmp/batch.log
+  id >> /tmp/batch.log
+  ls -ld /var/www/html >> /tmp/batch.log
+  ls -l /var/www/html >> /tmp/batch.log
   php /var/www/html/movie/saveTopMovieImage.php >> /tmp/batch.log 2>&1
   php /var/www/html/album/saveTopAlbumImage.php >> /tmp/batch.log 2>&1
   echo "end batch" >> /tmp/batch.log
