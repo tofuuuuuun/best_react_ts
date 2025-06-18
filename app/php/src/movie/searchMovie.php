e3052512285e218be2b2fa996248672cf0ec2cbb
hirano <hirakenlp21@gmail.com>
2025-06-18 21:59:07 +0900

    searchMovie関数でcURLリクエストのURLを動的に生成するために、movieTitleのURLエンコード処理を修正

--- app/php/src/movie/searchMovie.php
+++ app/php/src/movie/searchMovie.php
@@ -23,7 +23,7 @@ if ($cachedResult) {
 $curl = curl_init();
 
 curl_setopt_array($curl, [
-    CURLOPT_URL => "https://api.themoviedb.org/3/search/movie?query={$movieTitle}&include_adult=false&language=ja-JP&region=JA&page=1",
+    CURLOPT_URL => "https://api.themoviedb.org/3/search/movie?query={urlencode($movieTitle)}&include_adult=false&language=ja-JP&region=JA&page=1",
     // CURLOPT_URL => "https://api.themoviedb.org/3/search/movie?query=%E3%83%89%E3%83%A9%E3%81%88%E3%82%82%E3%82%93&include_adult=false&language=ja-JP&region=JA&page=1",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
