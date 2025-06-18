c57e1eaccd6efbb81233d1ee17d75332501db50d
hirano <hirakenlp21@gmail.com>
2025-06-18 21:25:52 +0900

    Access-Control-Allow-Originヘッダーを修正し、許可するオリジンを追加

--- app/php/src/movie/getTopRate.php
+++ app/php/src/movie/getTopRate.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-type: application/json; charset=utf-8;');
-header('Access-Control-Allow-Origin: http://localhost,https://dev.rahi-lab.com,,https://rahi-lab.com');
+header('Access-Control-Allow-Origin: http://localhost,https://dev.rahi-lab.com,https://rahi-lab.com');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
