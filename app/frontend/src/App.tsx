e41f6f039aeab966237cbae5fbab245d94e5f29e
hirano <hirakenlp21@gmail.com>
2026-08-09 20:20:44 +0900

    不要なクラスを削除し、Appコンポーネントの構造を整理。モーダルの状態管理を追加し、ボタンにクリックイベントを設定。index.htmlからクラスを削除し、SCSSファイルのテクスチャスタイルを削除。

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,40 +1,40 @@
 import { Header } from '@/common/Header';
 import albumLogoIcon from '@/images/album/logo.svg';
 import movieLogoIcon from '@/images/movie/logo.svg';
+import { useState } from 'react';
 
 export const App = () => {
-  return (
-    <>
-      <main>
-        <>
-          <Header />
-          <div className='l-main' id='wrapper'>
-            <article className='l-innerContainer'>
-              <div className='u-mb-2em'>
-                <h2 className='u-txt-white p-main__h2'>選ぶ、並べる、<br className='u-disp-sp' />見えてくる "好き"
-                </h2>
-              </div>
-              <div>
-                <p className='p-main__lead'>好きな作品を10個選ぶだけ。</p>
-                <p className='p-main__desc'>あなたを形作った作品たちを、ひとつのリストとして残せます。</p>
-              </div>
-            </article>
-          </div>
-          <ul className="c-select__list">
-            <li>
-              <button className="c-select__card u-bg-black">
-                <img src={albumLogoIcon} alt="音楽を選ぶ" />
-              </button>
-            </li>
+  const [isModalOpen, setModalIsOpen] = useState<boolean>(false);
 
-            <li>
-              <button className="c-select__card u-bg-black">
-                <img src={movieLogoIcon} alt="映画を選ぶ" />
-              </button>
-            </li>
-          </ul>
-        </>
-      </main >
-    </>
+  return (
+    <main>
+      <Header />
+      <div className='l-main' id='wrapper'>
+        <div className='l-innerContainer'>
+          <div className='u-mb-2em'>
+            <h2 className='u-txt-white p-main__h2'>選ぶ、並べる、<br className='u-disp-sp' />見えてくる "好き"
+            </h2>
+          </div>
+          <div>
+            <p className='p-main__lead'>好きな作品を10個選ぶだけ。</p>
+            <p className='p-main__desc'>あなたを形作った作品たちを、ひとつのリストとして残せます。</p>
+          </div>
+        </div>
+
+        <ul className="c-select__list">
+          <li>
+            <button className="c-select__card u-bg-black" onClick={() => setModalIsOpen(!isModalOpen)}>
+              <img src={albumLogoIcon} alt="音楽を選ぶ" />
+            </button>
+          </li>
+          <li>
+            <button className="c-select__card u-bg-black" onClick={() => setModalIsOpen(!isModalOpen)}>
+              <img src={movieLogoIcon} alt="映画を選ぶ" />
+            </button>
+          </li>
+        </ul>
+
+      </div>
+    </main >
   )
 }
--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -2,13 +2,9 @@ import { App } from '@/App'
 import '@/styles/style.scss'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
